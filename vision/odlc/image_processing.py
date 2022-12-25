20c2369cd7f6db16d66bf2768d0f371269a4bb3c
LightenedLimited <macnguyenanh@gmail.com>
2022-12-25 14:43:53 +0100

    moving to new file + formatting

---
+++ vision/odlc/image_processing.py
@@ -0,0 +1,236 @@
+import numpy as np
+import cv2
+
+
+def noise_removal(image):
+    """Using dilation, erosion, bluring and
+       morphology, remove small noise grains
+
+    Args:
+        image (cv2 image):
+
+    Returns:
+        cv2 image:
+    """
+    # taken from:
+    # https://github.com/wjbmattingly/ocr_python_textbook/blob/main/02_02_working%20with%20opencv.ipynb
+    kernel = np.ones((1, 1), np.uint8)
+    image = cv2.dilate(image, kernel, iterations=2)
+    kernel = np.ones((1, 1), np.uint8)
+    image = cv2.erode(image, kernel, iterations=2)
+    image = cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel)
+    image = cv2.medianBlur(image, 3)
+    return (image)
+
+
+def thin_font(image):
+    """Enlargens dark pixels via erosion,
+       making text "seem" thinner
+
+    Args:
+        image (cv2 image):
+
+    Returns:
+        cv2 image:
+    """
+    image = cv2.bitwise_not(image)
+    kernel = np.ones((2, 2), np.uint8)
+    image = cv2.erode(image, kernel, iterations=1)
+    image = cv2.bitwise_not(image)
+    return (image)
+
+
+def thick_font(image):
+    """Enlargens white pixels via dilation,
+       making text "seem" thicker
+
+    Args:
+        image (cv2 image):
+
+    Returns:
+        cv2 image:
+    """
+    image = cv2.bitwise_not(image)
+    kernel = np.ones((2, 2), np.uint8)
+    image = cv2.dilate(image, kernel, iterations=1)
+    image = cv2.bitwise_not(image)
+    return (image)
+
+
+def filter_contour(contours, image_width, image_height):
+    """Filter the largest 30 contours and those whose area
+    is more than 3% the image and less than 50% the image
+
+    Args:
+        contours (cv2 contours):
+        image_width (int):
+        image_height (int):
+
+    Returns:
+        list: list of filtered contours
+    """
+    # TODO: automate process, change values
+    # pick the largest 30 contours
+    if len(contours) > 30:
+        contours = list(sorted(
+            contours,
+            key=lambda contour: cv2.contourArea(contour),
+            reverse=True))
+        contours = contours[:30]
+
+    image_area = (image_height * image_width)
+
+    new_contours = list(filter(
+        lambda contour: cv2.contourArea(contour) / image_area > 0.03
+        and cv2.contourArea(contour) / image_area < 0.5,
+        contours))
+    return new_contours
+
+
+def extract_images_from_contour(image, contours):
+    """Given a contour, return the image inside the
+       rectangular bounding box of each contour
+
+    Args:
+        image (cv2 image): the in-color, original image without modifcations
+        contours (cv2 contours):
+
+    Returns:
+        list: a list of cropped images
+    """
+    output = []
+    image_height, image_width = image.shape[:2]
+    for contour in contours:
+        x, y, w, h = cv2.boundingRect(contour)
+        # TODO: confirm whether image bordering is needed
+        padding = 0
+        y -= padding
+        h += 2 * padding
+        x -= padding
+        w += 2 * padding
+
+        # making sure everything is in bounds still
+        y = max(y, 0)
+        x = max(x, 0)
+        h = min(h, image_height - 1)
+        w = min(w, image_width - 1)
+
+        new_image = image[y:y+h, x:x+w]
+        output.append(new_image)
+    return output
+
+
+def distance_from_pixel(pixel_a, pixel_b):
+    """Returns the square distance of one pixel to another
+
+    Args:
+        pixel_a (float tuple): RGB values
+        pixel_b (float tuple): RGB values
+
+    Returns:
+        float: the square distance
+    """
+    return (pixel_a[0] - pixel_b[0]) ** 2 + (pixel_a[1] - pixel_b[1]) ** 2 + \
+           (pixel_a[2] - pixel_b[2]) ** 2
+
+
+def cropped_images_to_binary(image, mean_pixel_value):
+    """Binarizes an image using k-means clustering, where k = 2
+
+    Args:
+        image (cv2 image):
+        mean_pixel_value (float tuple): an RGB tuple where the
+        values equal to the average values of the shape contour,
+        this is used to assign the appropriate label (either 0 or 1) to the
+        inside letter
+
+    Returns:
+        cv2 image: binarized cv2 image
+    """
+    # TODO: test variables, and change values
+    # The assumption for this function is that image is an image that
+    # primarily contains two colors, the background and letter color
+    # clustering is then possible as opposed to your normal binarization
+    flattened_image = np.float32(image.reshape((-1, 3)))
+
+    MAX_ITER = 10
+    EPS = 0.85
+    k = 2
+    criteria = (
+        cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER,
+        MAX_ITER,
+        EPS)
+
+    _, labels, centers = cv2.kmeans(
+        flattened_image,
+        k,
+        None,
+        criteria,
+        10,
+        cv2.KMEANS_RANDOM_CENTERS)
+    image_height, image_width = image.shape[:2]
+    mean_pixel_value = mean_pixel_value[:3]
+    labels = labels.reshape((image_height, image_width))
+    labels *= 255
+    if distance_from_pixel(centers[0], mean_pixel_value) > \
+       distance_from_pixel(centers[1], mean_pixel_value):
+        labels = 255 - labels
+    labels = np.array(labels, dtype=np.uint8)
+    return labels
+
+
+def image_with_border(image):
+    """Returns a cv2 image with either a black
+       or white border depending on the average of the
+       corners of the image
+
+    Args:
+        image (cv2 image):
+
+    Returns:
+        cv2 image: original image with border
+    """
+    # get average of 4 corners
+    height, width = image.shape[:2]
+    corners = int(image[0][0])
+    corners += int(image[0][width-1])
+    corners += int(image[height-1][0])
+    corners += int(image[height-1][width-1])
+    corners /= 4
+
+    # check if corners is closer to white than black
+    white = 255
+    black = 0
+    if corners < 255 / 2:
+        corners = black
+    else:
+        corners = white
+
+    bordersize = 10
+    image = cv2.copyMakeBorder(
+        image,
+        top=bordersize,
+        bottom=bordersize,
+        left=bordersize,
+        right=bordersize,
+        borderType=cv2.BORDER_CONSTANT,
+        value=corners
+    )
+
+    return image
+
+
+def average_color_contour(contour, image):
+    """Returns average pixel values of contour
+
+    Args:
+        contour (cv2 contour): _description_
+        image (cv2 image): _description_
+
+    Returns:
+        float tuple: (R value, G value, B vavue)
+    """
+    mask = np.zeros(image.shape[:2], np.uint8)
+    cv2.drawContours(mask, [contour], 0, 255, -1)
+    mean = cv2.mean(image, mask=mask)
+    return mean
--- vision/odlc/tesseract.py
+++ vision/odlc/tesseract.py
@@ -5,106 +5,26 @@ Light wrapper around Tesseract OCR model
 import pytesseract
 import cv2
 import os
-from odlc.cropper import crop_shape, crop_image_alpha
-import numpy as np
 from functools import reduce
+from odlc.image_processing import noise_removal, thin_font, thick_font, \
+    filter_contour, extract_images_from_contour, cropped_images_to_binary, \
+    image_with_border, average_color_contour
 
-# Assumes that the cropped image contains a single alphanumeric character
-# Takes the image, deskews, binarizes, pads image
-# Outputs a list of tuples containing a predicted character and confidence
-# Ideally, the maximum confidence character is the true character, but
-# if that character does not match any of the alphanumeric targets,
-# assume highest confidence character which does
-# @param cropped_image: cropped image filepath
-# @return list containing tuples of characters and our confidence
-
-def noise_removal(image):
-    #https://github.com/wjbmattingly/ocr_python_textbook/blob/main/02_02_working%20with%20opencv.ipynb
-    kernel = np.ones((1, 1), np.uint8)
-    image = cv2.dilate(image, kernel, iterations=2)
-    kernel = np.ones((1, 1), np.uint8)
-    image = cv2.erode(image, kernel, iterations=2)
-    image = cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel)
-    image = cv2.medianBlur(image, 3)
-    return (image)
-
-def thin_font(image):
-    image = cv2.bitwise_not(image)
-    kernel = np.ones((2,2),np.uint8)
-    image = cv2.erode(image, kernel, iterations=1)
-    image = cv2.bitwise_not(image)
-    return (image)
-
-def thick_font(image):
-    image = cv2.bitwise_not(image)
-    kernel = np.ones((2,2),np.uint8)
-    image = cv2.dilate(image, kernel, iterations=1)
-    image = cv2.bitwise_not(image)
-    return (image)
-
-def filter_contour(contours, image_width, image_height):
-    # TODO: automate process, change values
-    #pick the largest 30
-    if(len(contours) > 30):
-        contours = list(sorted(contours, key= lambda contour: cv2.contourArea(contour), reverse=True))
-        contours = contours[:30]
-
-    image_area = (image_height * image_width)
-
-    new_contours = list(filter(lambda contour: cv2.contourArea(contour) / image_area > 0.03 and cv2.contourArea(contour) / image_area < 0.5, contours))
-    return new_contours
-
-def extract_images_from_contour(image, contours):
-    output = []
-    image_height, image_width = image.shape[:2]
-    for contour in contours:
-        x,y,w,h = cv2.boundingRect(contour)
-        #TODO: add border
-        padding = 0
-        y -= padding
-        h += 2 * padding
-        x -= padding
-        w += 2 * padding
-
-        #making sure everything is in bounds still
-        y = max(y, 0)
-        x = max(x, 0)
-        h = min(h, image_height - 1)
-        w = min(w, image_width - 1)
-
-        new_image = image[y:y+h, x:x+w]
-        output.append(new_image)
-    return output
-
-
-def cropped_images_to_binary(image):
-    #TODO: k means clustering as a form of binarization, test variables
-    #The assumption for this function is that image is an image that
-    #primarily contains two colors, the background and letter color
-    #clustering is then possible as opposed to your normal binarization
-    flattened_image = np.float32(image.reshape((-1, 3)))
-
-    MAX_ITER = 10
-    EPS = 0.85
-    k = 2
-    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, MAX_ITER, EPS)
-    retval, labels, centers = cv2.kmeans(flattened_image, k, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS) 
-    image_height, image_width = image.shape[:2]
-    labels = labels.reshape((image_height, image_width))
-    labels *= 255
-    labels = np.array(labels, dtype=np.uint8)
-    return labels
 
 def generate_predictions(image):
     output = []
-    config_str = '--psm 10 -c tessedit_char_whitelist'
-    config_str += '=ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
+    image = image_with_border(image)
     height, width = image.shape[:2]
     center = (width // 2, height // 2)
     rotation_matrix_ninety = cv2.getRotationMatrix2D(center, 90, 1.0)
+
     for _ in range(0, 4):
         # get tesseract data from the image
         # we are interested in the recognized letter and its confidence
+        if os.getenv("DEBUG"):
+            cv2.imwrite("./images/debug/img-rotate{}.png".format(_), image)
+        config_str = '--psm 10 -c tessedit_char_whitelist'
+        config_str += '=ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890'
         data = pytesseract.image_to_data(
             image,
             config=config_str,
@@ -129,14 +49,33 @@ def generate_predictions(image):
             borderMode=cv2.BORDER_REPLICATE)
     return output
 
+
+def combine_similar_predictions(predictions):
+    seen_characters = {}
+    for prediction in predictions:
+        if prediction[0] in seen_characters:
+            seen_characters[prediction[0]] += prediction[1]
+        else:
+            seen_characters[prediction[0]] = prediction[1]
+    return list(seen_characters.items())
+
+# Outputs a list of tuples containing a predicted character and confidence
+# Ideally, the maximum confidence character is the true character, but
+# if that character does not match any of the alphanumeric targets,
+# assume highest confidence character which does
+# @param cropped_image: cropped image filepath
+# @return list containing tuples of characters and our confidence
+
+
 def get_matching_text(image):
     og_image = image.copy()
-    #TODO: automatted contrast correcting
+    # TODO: automatted contrast correcting
     # between 1-3
     contrast_multiplier = 2
     # between 0 - 100
     brightness_multiplier = 0
-    image = cv2.convertScaleAbs(image, alpha=contrast_multiplier, beta=brightness_multiplier)
+    image = cv2.convertScaleAbs(image, alpha=contrast_multiplier,
+                                beta=brightness_multiplier)
 
     image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     image = cv2.GaussianBlur(image, (5, 5), 0)
@@ -144,55 +83,73 @@ def get_matching_text(image):
     if os.getenv("DEBUG"):
         cv2.imwrite('./images/debug/img-ocr_blur.png', image)
 
-    image = cv2.adaptiveThreshold(image,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
-             cv2.THRESH_BINARY,7,1)
+    image = cv2.adaptiveThreshold(image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                                  cv2.THRESH_BINARY, 7, 1)
 
     if os.getenv("DEBUG"):
         cv2.imwrite('./images/debug/img-ocr_post_threshold.png', image)
-    
+
     image = noise_removal(image)
 
     if os.getenv("DEBUG"):
         cv2.imwrite('./images/debug/img-noise_removed.png', image)
 
-    thick_font_image = thick_font(image)
     image = thin_font(image)
 
     image = noise_removal(image)
 
     if os.getenv("DEBUG"):
+        thick_font_image = thick_font(image)
         cv2.imwrite('./images/debug/img-noise_thin.png', image)
         cv2.imwrite('./images/debug/img-noise_thick.png', thick_font_image)
 
-    contours, _ = cv2.findContours(image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    contours, _ = cv2.findContours(image, cv2.RETR_TREE,
+                                   cv2.CHAIN_APPROX_SIMPLE)
 
     if os.getenv("DEBUG"):
-        all_contours_image = cv2.drawContours(og_image.copy(), contours, -1, (0,255,0), 3)
+        all_contours_image = cv2.drawContours(og_image.copy(), contours, -1,
+                                              (0, 255, 0), 3)
         cv2.imwrite('./images/debug/img-all_contours.png', all_contours_image)
 
     image_height = image.shape[0]
     image_width = image.shape[1]
-    
+
     if os.getenv("DEBUG"):
         print("PRE FILTER: ", len(contours))
-    
+
     contours = filter_contour(contours, image_height, image_width)
-    
+
     if os.getenv("DEBUG"):
         print("POST FILTER: ", len(contours))
-        filtered_contour = cv2.drawContours(og_image.copy(), contours, -1, (0,255,0), 3)
-        cv2.imwrite('./images/debug/img-filtered_contours.png', filtered_contour)
-    
-    cropped_images = extract_images_from_contour(og_image, contours)
-    cropped_images = list(map(lambda crop_image: cropped_images_to_binary(crop_image), cropped_images))
-    if os.getenv("DEBUG"):
-        for index, crop_image in enumerate(cropped_images):
-            cv2.imwrite('./images/debug/crops/{}.png'.format(index), crop_image)
-    predictions = list(map(lambda crop_image: generate_predictions(crop_image), cropped_images))
-    print(predictions)
-    predictions = reduce(lambda prediction_a, prediction_b: prediction_a + prediction_b, predictions)
-    predictions = sorted(predictions, key=lambda prediction: prediction[1], reverse=True)
+        filtered_contour = cv2.drawContours(og_image.copy(), contours, -1,
+                                            (0, 255, 0), 3)
+        cv2.imwrite('./images/debug/img-filtered_contours.png',
+                    filtered_contour)
 
+    mean_contour_pixel_values = list(map(
+        lambda contour: average_color_contour(contour, og_image),
+        contours))
+    cropped_images = extract_images_from_contour(og_image, contours)
+    k_means_images = []
+    for index, crop_image in enumerate(cropped_images):
+        k_means_images.append(cropped_images_to_binary(
+            crop_image,
+            mean_contour_pixel_values[index]))
+
+    if os.getenv("DEBUG"):
+        for index, crop_image in enumerate(k_means_images):
+            cv2.imwrite('./images/debug/crops/{}.png'.format(index),
+                        crop_image)
+    predictions = list(map(
+        lambda crop_image: generate_predictions(crop_image),
+        k_means_images))
+    predictions = reduce(
+        lambda prediction_a, prediction_b: prediction_a + prediction_b,
+        predictions)
+    predictions = combine_similar_predictions(predictions)
+    predictions = sorted(
+        predictions,
+        key=lambda prediction: prediction[1], reverse=True)
     return predictions
 
 

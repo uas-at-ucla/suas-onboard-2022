a2d482187f01cb66b15aa7fe0140d822111945a6
Edwin Yu <edwinyyyu@gmail.com>
2023-01-27 18:31:34 -0800

    Fix plane-mission-basics.py formatting

--- flight/examples/plane-mission-basics.py
+++ flight/examples/plane-mission-basics.py
@@ -5,20 +5,21 @@ from pymavlink import mavutil
 
 METERS_PER_FEET = 0.3048
 
-ACC_RADIUS = 7.62 # 25 feet
+ACC_RADIUS = 7.62  # 25 feet
 
 CLIMB_ANGLE = 20
 DESCENT_ANGLE = 20
 
-AIRFIELD_ALT = 43.2816 # Airfield is 142 feet MSL
-MIN_RELATIVE_ALT = 22.86 # 75 feet
-MAX_RELATIVE_ALT = 121.92 # 400 feet
+AIRFIELD_ALT = 43.2816  # Airfield is 142 feet MSL
+MIN_RELATIVE_ALT = 22.86  # 75 feet
+MAX_RELATIVE_ALT = 121.92  # 400 feet
 
 TERMINATION_POINT = [38.315339, -76.548108]
 
 # TODO: Use correct connection string for real plane.
 connection_string = "127.0.0.1:14550"
 vehicle = connect(connection_string, wait_ready=True)
+
 
 # This function is for testing. TODO: REPLACE
 def arm_vehicle():
@@ -28,12 +29,14 @@ def arm_vehicle():
         print("Waiting for vehicle to arm . . .")
         time.sleep(1)
 
+
 # Puts the vehicle into AUTO mode.
 def start_mission():
     while vehicle.mode.name != "AUTO":
         vehicle.mode = VehicleMode("AUTO")
         print("Waiting for mode change to AUTO . . .")
         time.sleep(1)
+
 
 def mission_add_takeoff():
     cmds.download()
@@ -49,6 +52,7 @@ def mission_add_takeoff():
     )
     cmds.add(takeoff_command)
     cmds.upload()
+
 
 def mission_add_land(landing_point):
     cmds.download()
@@ -66,6 +70,7 @@ def mission_add_land(landing_point):
     cmds.add(land_command)
     cmds.upload()
 
+
 # Generate waypoints from waypoints given during competition.
 # Note: Altitudes are given in feet MSL(global)
 def generate_waypoint_list(filename):
@@ -81,6 +86,7 @@ def generate_waypoint_list(filename):
         waypoint[2] *= METERS_PER_FEET
         waypoint[2] -= AIRFIELD_ALT
     return waypoint_list
+
 
 def mission_add_waypoints(waypoint_list):
     cmds.download()
@@ -99,6 +105,7 @@ def mission_add_waypoints(waypoint_list):
         )
         cmds.add(waypoint_command)
     cmds.upload()
+
 
 arm_vehicle()
 

4eaa9023f45d8d89695652c155b4d1022338b724
Edwin Yu <edwinyyyu@gmail.com>
2023-04-14 19:53:18 -0700

    Implement basic keyboard attitude controls

--- flight/examples/plane-mission-basics.py
+++ flight/examples/plane-mission-basics.py
@@ -3,6 +3,7 @@ import time
 from dronekit import connect, Command, VehicleMode
 from pymavlink import mavutil
 
+import math
 import tkinter as tk
 
 METERS_PER_FEET = 0.3048
@@ -115,46 +116,84 @@ def mission_add_waypoints(waypoint_list):
     cmds.upload()
 
 
+def quaternion(roll=0, pitch=0, yaw=0):
+    """
+    Convert degrees to quaternions
+    """
+    t0 = math.cos(math.radians(yaw * 0.5))
+    t1 = math.sin(math.radians(yaw * 0.5))
+    t2 = math.cos(math.radians(roll * 0.5))
+    t3 = math.sin(math.radians(roll * 0.5))
+    t4 = math.cos(math.radians(pitch * 0.5))
+    t5 = math.sin(math.radians(pitch * 0.5))
+
+    w = t0 * t2 * t4 + t1 * t3 * t5
+    x = t0 * t3 * t4 - t1 * t2 * t5
+    y = t0 * t2 * t5 + t1 * t3 * t4
+    z = t1 * t2 * t4 - t0 * t3 * t5
+
+    return [w, x, y, z]
+
+
+def set_attitude_target(roll=0, pitch=0, yaw=0, thrust=0.5):
+    attitude_quaternion = quaternion(roll, pitch, yaw)
+    msg = vehicle.message_factory.set_attitude_target_encode(
+        0, 0, 0,
+        0b00000111,
+        attitude_quaternion,
+        0, 0, 0,
+        thrust
+    )
+    vehicle.send_mavlink(msg)
+
+
+# TODO: Allow combos, holding
 def press(event):
     k = event.keysym
     if k == "BackSpace":
-        print(vehicle.attitude)
         mode_switch("AUTO")
     elif k == "Escape":
         mode_switch("RTL")
-    elif k == "l":
-        cmds.clear()
-        cmds.upload()
-        mission_add_land(STARTING_POINT)
+    elif k == "g":
+        mode_switch("GUIDED")
     elif k == "w":  # Pitch down
-        pass
-        vehicle.channels.overrides["2"] = 0  # TODO
+        set_attitude_target(pitch=-20)
     elif k == "s":  # Pitch up
-        pass
+        set_attitude_target(pitch=20)
     elif k == "a":  # Roll left
-        pass
+        set_attitude_target(roll=-20)
     elif k == "d":  # Roll right
-        pass
+        set_attitude_target(roll=20)
+    elif k == "q":  # Yaw left
+        set_attitude_target(yaw=-20)
+    elif k == "e":  # Yaw right
+        set_attitude_target(yaw=20)
     elif k == "r":  # Throttle up
-        pass
+        set_attitude_target(thrust=1)
     elif k == "f":  # Throttle down
-        pass
+        set_attitude_target(thrust=0)
+    elif k == "i":
+        print(vehicle.channels["2"])
 
 
 def release(event):
     k = event.keysym
     if k == "w":
-        pass
+        set_attitude_target(pitch=0)
     elif k == "s":
-        pass
+        set_attitude_target(pitch=0)
     elif k == "a":
-        pass
+        set_attitude_target(roll=0)
     elif k == "d":
-        pass
+        set_attitude_target(roll=0)
+    elif k == "q":
+        set_attitude_target(yaw=0)
+    elif k == "e":
+        set_attitude_target(yaw=0)
     elif k == "r":
-        pass
+        set_attitude_target(thrust=0.5)
     elif k == "f":
-        pass
+        set_attitude_target(thrust=0.5)
 
 
 arm_vehicle()

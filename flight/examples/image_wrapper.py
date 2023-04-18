5c0f7c469b4ffa6b9a9736d6beefacefc489bcb2
Alexander Thaik <ajt.school@gmail.com>
2023-04-17 18:16:39 -0700

    Added image wrapper script

---
+++ flight/examples/image_wrapper.py
@@ -0,0 +1,51 @@
+import requests
+import time
+
+
+def index():
+    response = requests.get('http://localhost:8003/index')
+    if response.status_code == 200:
+        index = response.json()
+    return index
+
+
+def get_best_object_detections():
+    response = requests.get('http://localhost:8003/odlc')
+    if response.status_code == 200:
+        detections = response.json()
+    return detections
+
+
+def queue_image_for_odlc(image_png):
+    while True:
+        response = requests.post(
+            'http://localhost:8003/odlc', data={'image': image_png})
+        if response.status_code == 200:
+            print('Image queued')
+        time.sleep(0.25)
+
+
+def update_telemetry(altitude, latitude, longitude, heading):
+    response = requests.post('http://localhost:8003/telemetry',
+                             json={'altitude': altitude, 'latitude': latitude,
+                                   'longitude': longitude, 'heading': heading})
+    if response.status_code == 200:
+        print("Telemetry updated")
+
+
+def update_targets(type, shape_color, text_color, text, shape):
+    response = requests.post('http://localhost:8003/targets',
+                             json={'type': type,
+                                   'class': {'shape-color': shape_color,
+                                             'text-color': text_color,
+                                             'text': text,
+                                             'shape': shape}})
+    if response.status_code == 200:
+        print("Targets updated")
+
+
+def get_status():
+    response = requests.get('http://localhost:8003/status')
+    if response.status_code == 200:
+        status = response.json()
+    return status
